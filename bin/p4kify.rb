5597777fb99faaa4e8b683e6e092873fca9932a0
Chris Dehghanpoor <chris.dehghanpoor@lookout.com>
2015-06-04 13:42:43 -0700

    Update p4kify.rb to read email username and password from config file; fetch album art from Pitchfork rather than Spotify

--- bin/p4kify.rb
+++ bin/p4kify.rb
@@ -4,22 +4,28 @@ require 'nokogiri'
 require 'json'
 require 'mail'
 require 'optparse'
+require 'yaml'
 
-options = {}
+OPTIONS = {}
 OptionParser.new do | opts |
-  opts.banner = "Usage: p4kify.rb [options]"
+  opts.banner = "Usage: p4kify.rb [OPTIONS]"
+	OPTIONS[:config] = YAML.load_file(ENV['HOME'] + "/.p4kify.conf")
 
   opts.on("-t", "--to-email [EMAIL]", "The email address the report will be mailed to") do |t|
-    options[:to] = t
+    OPTIONS[:to] = t
    end
 
   opts.on("-f", "--from-email [EMAIL]", "The email address the report will be mailed from") do |f|
-    options[:from] = f
+    OPTIONS[:from] = f
   end
 
   opts.on("-n", "--name [NAME]", "The name used in the email's greeting") do |n|
-    options[:name] = n
+    OPTIONS[:name] = n
   end
+
+	opts.on("-c", "--config [CONFIG]", "The path to your p4kify.conf file. Defaults to $HOME/.p4kify.conf") do |c|
+		OPTIONS[:config] = YAML.load_file(c)
+	end
 
   opts.on_tail("-h", "--help", "Displays this information, ya dingus") do
     puts opts
@@ -27,20 +33,23 @@ OptionParser.new do | opts |
   end
 end.parse!
 
-P4kAlbum = Struct.new(:artist, :album_name, :blurb, :review_url, :review_score, :best_new_music?, :spotify_url, :artwork_url, :on_spotify?)
+abort("Missing required arguments") if OPTIONS[:to].nil? || OPTIONS[:from].nil? || OPTIONS[:name].nil?
+
+
+P4kAlbum = Struct.new(:artist, :album_name, :artwork_url, :blurb, :review_url, :review_score, :best_new_music?, :spotify_url, :on_spotify?)
 
 def send_mail_via_gmail(to_addr, from_addr, msg_subject, msg_body)
   options = { 
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => '',
-    :password             => '',
+		:user_name            => OPTIONS[:config]["email_user_name"],
+    :password             => OPTIONS[:config]["email_password"],
     :authentication       => 'plain',
     :enable_starttls_auto => true  
   }
 
 
-  abort("Must enter your gmail username and password into the script before it will work. Aborting.") if options[:user_name].empty? && options[:password].empty?
+  abort("Must enter your gmail username and password into the script before it will work. Aborting.") if options[:user_name].empty? && OPTIONS[:password].empty?
 
   Mail.defaults do
     delivery_method :smtp, options
@@ -62,12 +71,13 @@ def crawl_p4k
   p4k.css('#review-day-1').css('.review-detail-info').map do |elem| 
     artist = elem.css('a h1').text.strip
     album = elem.css('a h2').text.strip
+		artwork_url = elem.parent.css('.review-cover a div')[0].attr('data-content').match(/src="(.*)"/)[1]
     blurb = elem.css('.content-container').text.strip
     review_url = "http://pitchfork.com#{elem.css('a')[0]['href'].strip}"
     review_page = Nokogiri::HTML(open(review_url))
     review_score = review_page.css('.score').text.strip
     best_new_music = review_page.css('.bnm-label').text.strip == "Best New Music" ? true : false
-    P4kAlbum.new(artist, album, blurb, review_url, review_score, best_new_music)
+    P4kAlbum.new(artist, album, artwork_url, blurb, review_url, review_score, best_new_music)
   end
 end
 
@@ -76,7 +86,6 @@ def crawl_spotify(todays_reviews)
     spotify_result_hash = JSON.parse(open("https://api.spotify.com/v1/search?q=album:#{URI.encode(album.album_name)}%20artist:#{URI.encode(album.artist)}&type=album&market=US").read)["albums"]["items"][0]
     if spotify_result_hash
       album.spotify_url = spotify_result_hash["external_urls"]["spotify"]
-      album.artwork_url = spotify_result_hash["images"][1]["url"]
       album[:on_spotify?] = true
     else
       album[:on_spotify?] = false
@@ -100,11 +109,11 @@ review_text = todays_album_reviews.reduce("") do |acc, album|
   acc
 end
 
-msg = """<html><body>Good morning #{options[:name]},<br>
+msg = """<html><body>Good morning #{OPTIONS[:name]},<br>
 <br>
 Here are today's latest Pitchfork reviews!<br>
 <br>
 #{review_text}
 </body></html>"""
 
-send_mail_via_gmail(options[:to], options[:from], "Today's Pitchfork Reviews", msg)
+send_mail_via_gmail(OPTIONS[:to], OPTIONS[:from], "Today's Pitchfork Reviews", msg)
